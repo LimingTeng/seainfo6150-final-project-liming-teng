2a7ab0a8e7924d780955ae180e7701bc58c2798f
Liming Teng <tengliming@LimingdeMacBook-Pro.local>
2020-04-06 13:51:18 -0700

    update

--- src/reducers/reducer.jsx
+++ src/reducers/reducer.jsx
@@ -17,59 +17,20 @@ export default (
 ) => {
   switch (action.type) {
     case 'SELECT_PRODUCT':
-      let selectedOptions;
-
-      const selectedProduct = state.products[action.payload.id];
-      switch (selectedProduct.categoryId) {
-        case 'limousine':
-          selectedOptions = {
-            numSeats: 8
-          }
-          break;
-        case 'sportsCar':
-          selectedOptions = {
-            numSeats: 2
-          }
-          break;
-        case 'taxi':
-          selectedOptions = {
-            color: '#ffff00'
-          }
-          break;
-        case 'fireEngine':
-          selectedOptions = {
-            color: '#ff0000',
-            numSeats: 2,
-            hasTintedWindows: false,
-            hasRadio: false,
-            hasCigaretteLighters: false
-          }
-          break;
-        case 'jeep':
-          selectedOptions = {
-            hasTintedWindows: false,
-            hasAirConditioning: false
-          }
-          break;
-        default:
-          selectedOptions = {}
-      }
-
       return {
         ...state,
         selectedProductId: action.payload.id,
-        selectedOptions
-      }
+      };
     case 'VIEW_PRODUCT':
       return {
         ...state,
         viewedProducts: [action.payload.id, ...state.viewedProducts]
-      }
+      };
     case 'REMOVE_OPTION':
       return {
         ...state,
         selectedOptions: omit(state.selectedOptions, [action.payload.id])
-      }
+      };
     case 'SET_OPTION':
       return {
         ...state,
@@ -77,7 +38,7 @@ export default (
           ...state.selectedOptions,
           ...action.payload
         }
-      }
+      };
     case 'SET_INFO':
       return {
         ...state,
@@ -85,12 +46,12 @@ export default (
           ...state.userInfo,
           ...action.payload
         }
-      }
+      };
     case 'SET_ERROR':
       return {
         ...state,
         error: action.payload.error
-      }
+      };
     case 'REMOVE_ERROR':
       return omit(state, ['error'])
     default:
